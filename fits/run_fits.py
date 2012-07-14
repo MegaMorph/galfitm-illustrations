df22e0c9b3edf1be1c4d966a4914bde2d7d0d236
Steven Bamford <steven@stevenbamford.com>
2012-07-14 23:20:53 +0100

    Added python code to run illustration galfit fits

---
+++ fits/run_fits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from glob import glob
+import os
+
+def run_all_fits():
+    ids = glob('*/')
+    for id in ids:
+        run_fits(id)
+
+def run_fits(id='A1'):
+    os.chdir(id)
+    feedmes = glob('fit*galfit')
+    for f in feedmes:
+        os.system('galfit %s > galfit.%s.out; if [ $? -eq 0 ]; then echo %s: success; else echo %s: failure; fi'%(f,f,f,f))
+    os.chdir('..')
+
+if __name__ =='__main__':
+    run_all_fits()
