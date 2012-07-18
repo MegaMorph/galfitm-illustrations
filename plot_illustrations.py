416db9d882e11855fd6d020fc67dee6a0ef0a20f
Steven Bamford <steven@stevenbamford.com>
2012-07-18 13:50:50 +0100

    Improve illustration plotting

--- plot_illustrations.py
+++ plot_illustrations.py
@@ -16,10 +16,25 @@ w = numpy.array([3543,4770,6231,7625,9134,10305,12483,16313,22010], numpy.float)
 
 xlim = (2000, 23000)
 
-simmag_std = numpy.array([16.935,15.964,15.0,14.562,14.267,14.183,13.992,13.672,13.547])
-simmag_half = numpy.array([17.687,16.717,15.753,15.315,15.019,14.936,14.745,14.425,14.299])
-simmag_disk = numpy.array([17.328,16.509,15.753,15.374,15.112,15.056,14.882,14.597,14.5])
-simmag_bulge = numpy.array([18.229,16.974,15.753,15.258,14.934,14.827,14.623,14.276,14.13])
+sim_std = {'MAG': numpy.array([16.935,15.964,15.0,14.562,14.267,14.183,13.992,13.672,13.547])}
+sim_A_disk = {'MAG': numpy.array([17.687,16.717,15.753,15.315,15.019,14.936,14.745,14.425,14.299]),
+              'Re': numpy.array([6.0]*9), 'n': numpy.array([1.0]*9),
+              'AR': numpy.array([0.4]*9), 'PA': numpy.array([45.0]*9)}
+sim_A_bulge = {'MAG': numpy.array([17.687,16.717,15.753,15.315,15.019,14.936,14.745,14.425,14.299]),
+              'Re': numpy.array([3.0]*9), 'n': numpy.array([4.0]*9),
+              'AR': numpy.array([0.8]*9), 'PA': numpy.array([45.0]*9)}
+sim_D_disk = {'MAG': numpy.array([17.328,16.509,15.753,15.374,15.112,15.056,14.882,14.597,14.5]),
+              'Re': numpy.array([6.0]*9), 'n': numpy.array([1.0]*9),
+              'AR': numpy.array([0.4]*9), 'PA': numpy.array([45.0]*9)}
+sim_D_bulge = {'MAG': numpy.array([18.229,16.974,15.753,15.258,14.934,14.827,14.623,14.276,14.13]),
+               'Re': numpy.array([3.0]*9), 'n': numpy.array([4.0]*9),
+               'AR': numpy.array([0.8]*9), 'PA': numpy.array([45.0]*9)}
+sim_E_disk = {'MAG': numpy.array([16.999,16.127,15.753,15.529,15.389,15.41,15.384,15.192,15.281]),
+              'Re': numpy.array([6.0]*9), 'n': numpy.array([1.0]*9),
+              'AR': numpy.array([0.8]*9), 'PA': numpy.array([45.0]*9)}
+sim_E_bulge = {'MAG': numpy.array([18.546,17.519,15.753,15.084,14.764,14.623,14.433,14.02,13.78]),
+               'Re': numpy.array([3.0]*9), 'n': numpy.array([4.0]*9),
+               'AR': numpy.array([1.0]*9), 'PA': numpy.array([45.0]*9)}
 
 marker = ['o', '^', 's', 'D', 'x', '+', '*']
 
@@ -27,7 +42,7 @@ ylim_std = {'MAG': (17.51, 12.49), 'Re': (0.01, 10.99), 'n': (0.01, 5.99),
             'AR': (0.41, 0.79), 'PA': (35.01, 64.99)}
 
 ylim_disk = {'MAG': (18.51, 13.49), 'Re': (3.01, 8.99), 'n': (0.01, 2.99),
-             'AR': (0.21, 0.59), 'PA': (35.01, 64.99)}
+             'AR': (0.21, 0.89), 'PA': (35.01, 64.99)}
 
 ylim_bulge = {'MAG': (18.51, 13.49), 'Re': (0.01, 5.99), 'n': (2.01, 7.99),
               'AR': (0.61, 1.09), 'PA': (0.01, 89.99)}
@@ -39,26 +54,26 @@ labels = {'MAG': '$m$', 'Re': '$R_e$', 'n': '$n$', 'AR': '$b/a$', 'PA': '$\\thet
 wlfuncs = {'A1c': numpy.log10, 'Ah1c': numpy.log10}
 
 def plot_all():
-    plot(('A2', 'A1'), 1, '1', 'True')
-    plot(('Ah2', 'Ah1'), 1, '2', 'True')  # and fit 3
-    plot(('Bh2', 'Bh1'), 1, '3', 'True')  # and fit 3
-    plot(('A1c', 'A1'), 1, '4', 'True')  # add additional wavelength scale
-    plot(('Ah1c', 'Ah1'), 1, '4h', 'True')  # add additional wavelength scale
-    plot(('A1', 'A1a', 'A1b'), 1, '5', 'True', varlist=('MAG',))
-    plot(('Ah1', 'Ah1a', 'Ah1b'), 1, '5h', 'True', varlist=('MAG',))
-    # plot(('A1', 'A1c', 'A1d'), 1, '6', 'True')
-    # plot(('Ah1', 'Ah1c', 'Ah1d'), 1, '6h', 'True')
+    plot(('A2', 'A1'), 1, '01', 'True')
+    plot(('Ah2', 'Ah1'), 1, '02', 'True')  # and fit 3
+    plot(('Bh2', 'Bh1'), 1, '03', 'True')  # and fit 3
+    plot(('A1c', 'A1'), 1, '04', 'True')  # add additional wavelength scale
+    plot(('Ah1c', 'Ah1'), 1, '04h', 'True')  # add additional wavelength scale
+    plot(('A1', 'A1a', 'A1b'), 1, '05', 'True', varlist=('MAG',))
+    plot(('Ah1', 'Ah1a', 'Ah1b'), 1, '05h', 'True', varlist=('MAG',))
+    # plot(('A1', 'A1c', 'A1d'), 1, '06', 'True')
+    # plot(('Ah1', 'Ah1c', 'Ah1d'), 1, '06h', 'True')
     # illustration 7 requires a different kind of plot
-    plot(('D1',), 1, '8', 'True')  # and D2 and D3
-    plot(('A4', 'A5'), 1, '9-1', 'True', ylim=ylim_bulge, simmag=simmag_half) # and A6
-    plot(('A4', 'A5'), 2, '9-2', 'True', ylim=ylim_disk, simmag=simmag_half) # and A6
-    plot(('D4', 'D5'), 1, '10-1', 'True', ylim=ylim_bulge, simmag=simmag_bulge) # and D6
-    plot(('D4', 'D5'), 2, '10-2', 'True', ylim=ylim_disk, simmag=simmag_disk) # and D6
-    plot(('E4', 'E5'), 1, '11-1', 'True', ylim=ylim_bulge, simmag=simmag_bulge) # and E6
-    plot(('E4', 'E5'), 2, '11-2', 'True', ylim=ylim_disk, simmag=simmag_disk) # and E6
+    plot(('D1',), 1, '08', 'True')  # and D2 and D3
+    plot(('A4', 'A5'), 1, '09-1', 'True', ylim=ylim_bulge, sim=sim_A_bulge) # and A6
+    plot(('A4', 'A5'), 2, '09-2', 'True', ylim=ylim_disk, sim=sim_A_disk) # and A6
+    plot(('D4', 'D5'), 1, '10-1', 'True', ylim=ylim_bulge, sim=sim_D_bulge) # and D6
+    plot(('D4', 'D5'), 2, '10-2', 'True', ylim=ylim_disk, sim=sim_D_disk) # and D6
+    plot(('E4', 'E5'), 1, '11-1', 'True', ylim=ylim_bulge, sim=sim_E_bulge) # and E6
+    plot(('E4', 'E5'), 2, '11-2', 'True', ylim=ylim_disk, sim=sim_E_disk) # and E6
     
 def plot(id=('A2', 'A1'), compno=1, name='0', show_func=False,
-         varlist=varlist_std, ylim=ylim_std, simmag=simmag_std):
+         varlist=varlist_std, ylim=ylim_std, sim=sim_std):
     print name, ':', id
     res = [fit_results(i) for i in id]
     if show_func:
@@ -66,12 +81,12 @@ def plot(id=('A2', 'A1'), compno=1, name='0', show_func=False,
     else:
         func = None
     nvar = len(varlist)
-    fig = pyplot.figure(figsize=(5, nvar*3))
+    fig = pyplot.figure(figsize=(5, 15))
     fig.subplots_adjust(bottom=0.05, top=0.94, left=0.2, right=0.95, hspace=0.05)
     for i, v in enumerate(varlist):
-        ax = make_bands_plot(fig, (nvar, 1, i+1), labels[v], i==0, i==nvar-1)
-        if v == 'MAG':
-            pyplot.plot(w, simmag, '-xk')
+        ax = make_bands_plot(fig, (5, 1, i+1), labels[v], i==0, i==nvar-1)
+        if v in sim.keys():
+            pyplot.plot(w, sim[v], '-xk')
         plotres(res, id, 'COMP%i_%s'%(compno, v), func)
         pyplot.ylim(ylim[v])
         if i==0:
