74f724d96a52fe4eb2ac0bc4c4b8c33fc2048467
Steven Bamford <steven@stevenbamford.com>
2012-10-22 12:03:53 +0100

    Fix typo

--- models/make_images.py
+++ models/make_images.py
@@ -40,7 +40,7 @@ if __name__ =='__main__':
     make_images('B', 5)
     make_images('B', 50, ['H'])
     for x in 'abcdefghi':
-        make_images('C'+x, 0, ['r'])
+        make_images('C'+x, 0.01, ['r'])
         make_images('C'+x, 5, ['r'])
     make_images('D', 0.01)
     make_images('D', 5)
