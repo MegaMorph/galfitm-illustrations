35af67eb270c5ee177eb264c339c6f9dd390a288
Steven Bamford <steven@stevenbamford.com>
2014-06-11 22:24:07 +0100

    Make sure all fit feedmes get made

--- fits/make_fit_feedmes.py
+++ fits/make_fit_feedmes.py
@@ -13,9 +13,10 @@ def make_feedmes():
         feedmes = glob('fit*diff')
         # output starting models
         for f in feedmes:
-            template = r'fit(.).*(\d)(n|m){0,1}([ugrizYJHK]{0,1})([abcde]{0,1})'
+            template = r'fit(.*)(\d)(n|m){0,1}([ugrizYJHK]{0,1})([abcde]{0,1})'
             matchobj = re.match(template, f)
-            if matchobj.group(1) != 'A':
+            print matchobj.groups()
+            if matchobj.group(1) != 'A' or matchobj.group(5) != '':
                 cmd = matchobj.expand('patch -o \g<0>.galfit ../A\g<2>/'
                                       'fitA\g<2>\g<4>.galfit \g<0>.diff')
                 print cmd
