4d6553f339473b67491b19a502aad9f469ccb6b9
Steven Bamford <steven@stevenbamford.com>
2014-06-11 22:25:13 +0100

    Remove debug prints

--- fits/make_fit_feedmes.py
+++ fits/make_fit_feedmes.py
@@ -15,11 +15,9 @@ def make_feedmes():
         for f in feedmes:
             template = r'fit(.*)(\d)(n|m){0,1}([ugrizYJHK]{0,1})([abcde]{0,1})'
             matchobj = re.match(template, f)
-            print matchobj.groups()
             if matchobj.group(1) != 'A' or matchobj.group(5) != '':
                 cmd = matchobj.expand('patch -o \g<0>.galfit ../A\g<2>/'
                                       'fitA\g<2>\g<4>.galfit \g<0>.diff')
-                print cmd
                 os.system(cmd)
         os.chdir('..')
 
