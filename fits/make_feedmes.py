c596ceff8d47ea916400184d6a38f651d79bf5d7
Steven Bamford <steven@stevenbamford.com>
2014-06-11 22:02:11 +0100

    Feedme diffs working

--- fits/make_feedmes.py
+++ fits/make_feedmes.py
@@ -13,12 +13,13 @@ def make_feedmes():
         feedmes = glob('fit*diff')
         # output starting models
         for f in feedmes:
-            template = r'fit.+(\d)(n|m){0,1}([ugrizYJHK]{0,1})([abcde]{0,1})'
+            template = r'fit(.).*(\d)(n|m){0,1}([ugrizYJHK]{0,1})([abcde]{0,1})'
             matchobj = re.match(template, f)
-            cmd = matchobj.expand('patch -o \g<0>.galfit ../A\g<1>/'
-                                  'fitA\g<1>\g<3>.galfit \g<0>.diff')
-            print cmd
-            os.system(cmd)
+            if matchobj.group(1) != 'A':
+                cmd = matchobj.expand('patch -o \g<0>.galfit ../A\g<2>/'
+                                      'fitA\g<2>\g<4>.galfit \g<0>.diff')
+                print cmd
+                os.system(cmd)
         os.chdir('..')
 
 if __name__ =='__main__':
