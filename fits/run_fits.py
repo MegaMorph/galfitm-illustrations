e409921a88fcdee743e3f2c000a77703a623288c
Steven Bamford <steven@stevenbamford.com>
2017-08-04 16:54:05 +0100

    More sensible parallel fits

--- fits/run_fits.py
+++ fits/run_fits.py
@@ -10,6 +10,7 @@ def run_all_fits(idglob='*'):
     poller = select.poll()
     subprocs = {} # map stdout pipe's file descriptor to the Popen object
     ids = glob(idglob+'/')
+    commands = []
     for id in ids:
         os.chdir(id)
         feedmes = glob('fit*galfit')
@@ -17,30 +18,32 @@ def run_all_fits(idglob='*'):
         for f in feedmes:
             s = f.replace('fit', 'start', 1)
             sfn = s.replace('.galfit', '.fits')
-            cmd = 'nice galfit -o1 -f %s %s > %s.out; if [ $? -eq 0 ]; then echo %s: success; else echo %s: failure; fi'%(sfn,f,s,s,s)
-            subproc = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            subprocs[subproc.stdout.fileno()] = subproc
-            poller.register(subproc.stdout, select.POLLHUP)
+            cmd = 'cd %s; nice galfit -o1 -f %s %s > %s.out; if [ $? -eq 0 ]; then echo %s: success; else echo %s: failure; fi'%(id,sfn,f,s,s,s)
+            commands.append(cmd)
         # do proper fits
         for f in feedmes:
-            cmd = 'nice galfit %s > %s.out; if [ $? -eq 0 ]; then echo %s: success; else echo %s: failure; fi'%(f,f,f,f)
-            subproc = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            subprocs[subproc.stdout.fileno()] = subproc
-            poller.register(subproc.stdout, select.POLLHUP)
+            cmd = 'cd %s; nice galfit %s > %s.out; if [ $? -eq 0 ]; then echo %s: success; else echo %s: failure; fi'%(id,f,f,f,f)
+            commands.append(cmd)
         os.chdir('..')
     #loop that polls until completion
     while True:
-        for fd, flags in poller.poll(5000): # never more than five seconds without a UI update
-            done_proc = subprocs[fd]
-            del subprocs[fd]
-            poller.unregister(fd)
-            print '\r\033[K'+done_proc.communicate()[0],
-        print '.',
-        sys.stdout.flush()
-        #don't forget to break when all are done
-        if len(subprocs) == 0:
-            print '\r\033[KAll complete'
-            break
+        if len(commands) > 0 and len(subprocs) < 10:
+            cmd = commands.pop(0)
+            subproc = subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            subprocs[subproc.stdout.fileno()] = subproc
+            poller.register(subproc.stdout, select.POLLHUP)
+        else:
+            for fd, flags in poller.poll(5000): # never more than five seconds without a UI update
+                done_proc = subprocs[fd]
+                del subprocs[fd]
+                poller.unregister(fd)
+                print '\r\033[K'+done_proc.communicate()[0],
+                print '.',
+                sys.stdout.flush()
+                #don't forget to break when all are done
+            if len(subprocs) == 0:
+                print '\r\033[KAll complete'
+                break
 
 
 if __name__ =='__main__':
@@ -48,4 +51,3 @@ if __name__ =='__main__':
         run_all_fits(sys.argv[1])
     else:
         run_all_fits()
-
